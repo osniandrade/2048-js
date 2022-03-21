1c53f9a56a6516d7779e8588ead690fdbf4732a5
osni junior <osniandrade@gmail.com>
2022-03-21 16:53:43 -0300

    first push

---
+++ script.js
@@ -0,0 +1,70 @@
+import Grid from "./Grid.js"
+import Tile from "./Tile.js"
+
+const gameBoard = document.getElementById("game-board");
+
+const grid = new Grid(gameBoard);
+
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+setupInput();
+console.log(grid.cellByColumn);
+
+function setupInput() {
+	window.addEventListener("keydown", handleInput, { once : true });
+}
+
+function handleInput(e) {
+	console.log(e.key);
+	switch(e.key) {
+		case "ArrowUp":
+			moveUp();
+			break;
+		case "ArrowDown":
+			moveDown();
+			break;
+		case "ArrowLeft":
+			moveLeft();
+			break;
+		case "ArrowRight":
+			moveRight();
+			break;
+		default:
+			setupInput();
+			return;
+	}
+
+	// Other code
+
+	setupInput();
+}
+
+function moveUp() {
+	return slideTiles(grid.cellsByColumn);
+}
+
+function slideTiles(cells) {
+	cells.forEach(group => {
+		for (let i = 1; i < group.length; i++) {
+			const cell = group[i];
+			if (cell.tile == null)
+				continue;
+			let lastValidCell;
+			for (let j = i - 1; j >= 0; j--) {
+				const moveToCell = group[j];
+				if (!moveToCell.canAccept(cell.tile)) 
+					break;
+				lastValidCell = moveToCell;
+			}
+			if (lastValidCell != null) {
+				if (lastValidCell.tile != null) {
+					lastValidCell.mergeTile = cell.tile;
+				} else {
+					lastValidCell.tile = cell.tile;
+				}
+				cell.tile = null;
+			}
+		}
+	})
+
+}
